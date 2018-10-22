87a096730b66e3fcb6492689381b53589fe7dfbe
Fathur <fathur@telectg.co.id>
2018-10-22 09:32:17 +0700

    adjusting view privacy policy

--- resources/views/page/privacy.blade.php
+++ resources/views/page/privacy.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="container posts">
+        <div class="row posts-show">
             <div class="col-sm-12">
                 @include('page.privacy-policy')
             </div>
